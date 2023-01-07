6190161d0dff126f4d7071f8fc83d06ad5188257
Jennifer Smith <jennifernsmithco@gmail.com>
2023-01-07 10:24:02 -0700

    first commit

---
+++ modulejupy.py
@@ -0,0 +1,13 @@
+def sqfthouse (length, width):
+    """calculates the square footage of a house"""
+    area = length * width
+    return area
+
+def circumference_circle(radius):
+    """calculates the circumference of a circle"""
+    circumference = 2*3.14*radius
+    return circumference
+    
+print(sqfthouse(3,7))
+
+print(circumference_circle(4))
